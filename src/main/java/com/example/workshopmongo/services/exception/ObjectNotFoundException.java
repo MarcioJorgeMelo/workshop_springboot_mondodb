d05b5ab42dcf02b2d3f1642c4d2cf2b924f94f83
MarcioJorgeMelo <142255861+MarcioJorgeMelo@users.noreply.github.com>
2024-09-09 16:12:00 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/example/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+
+        super(msg);
+
+    }
+
+}
